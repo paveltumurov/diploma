e0fd1c82d29357067a05bfaf28c0237fbaaae706
Pavel Tumurov <paveltumurov260@gmail.com>
2026-03-10 23:54:08 +0300

    Добавлены все реализации парсеров: standard, eastar, hd

---
+++ src/HdGSE/HdParser.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../../include/IFaces/IGseParser.h"
+class HdGSEParser: public IParser{
+  private:
+
+    uint16_t getBzReservedLength(std::uint8_t b1, std::uint8_t b2) const;
+    uint16_t get12bitLength(std::uint8_t b1, std::uint8_t b2) const;
+    bool isGsePadding(std::uint8_t b1, std::uint8_t b2) const;
+
+    bool isStartFragment(unsigned char b1) const;
+    bool isEndFragment(unsigned char b1) const;
+
+    enum class GseMode {
+        Default = 0b00,
+        Guarantee = 0b01,
+        NCR = 0b10,
+        CryptorAndGuarantee = 0b11
+    };
+    GseMode getGseMode(uint8_t b1) const;
+
+    GSEStatistics stats;
+
+    SOCKET sendSocket;
+    sockaddr_in destinationAddress;
+
+    bool forwardingEnabled;
+
+  public:
+
+    HdGSEParser();
+    HdGSEParser(SOCKET sock, sockaddr_in addr);
+
+    void processBBF(const char* buffer, int size);
+    void sendPayload(const char* buffer, int size);
+    void printStatistics() const;
+
+    ~HdGSEParser() override;
+};
---
+++ src/Standard/GseParser.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <winsock2.h>
+#include "../../include/IFaces/IGseParser.h"
+
+class StandardGSEParser: public IParser{
+  private:
+    uint16_t get12bitLength(std::uint8_t b1, std::uint8_t b2) const;
+    bool isGSEPadding(std::uint8_t byte) const;
+    uint8_t getLabelType(uint8_t b1) const;
+    bool isStartFragment(uint8_t b1) const;
+    bool isEndFragment(uint8_t b1) const;
+    int getHeaderLength(uint8_t b1, uint8_t b2) const;
+
+    SOCKET sendSocket;
+    sockaddr_in destinationAddress;
+
+    bool forwardingEnabled;
+
+  public:
+    GSEStatistics stats;
+
+    StandardGSEParser();
+    
+    StandardGSEParser(SOCKET sock, sockaddr_in addr);
+
+    void processBBF(const char* buffer, int32_t size);
+    void printStatistics() const;
+    ~StandardGSEParser() override;
+    void sendPayload(const char* buffer, int size);
+};
+
